979aa7b0bd2db920ace99de913eb3a5cf2ea175c
Mario Areias <mario.s.s.areias@gmail.com>
2023-12-02 16:26:34 +1100

    day 1 working

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,12 @@ fn main() {
     let f = open_file("input.txt".to_string()).unwrap();
     let lines = read_lines_from_file(f).unwrap();
 
-    let mut ic: char;
-    let mut ec: char;
-
-    let mut ii: u32;
-    let mut ei: u32;
+    let mut first: u32;
+    let mut last: u32;
 
     let mut i: usize;
 
     let mut total = 0;
-    let mut foundi: bool;
-    let mut founde: bool;
 
     let numbers: Vec<String> = vec![
         "one".to_string(),
@@ -40,13 +35,10 @@ fn main() {
     ];
 
     for line in lines {
-        ic = 'a';
-        ec = 'a';
-
-        foundi = false;
-        founde = false;
-
         i = 0;
+
+        first = 0;
+        last = 0;
 
         let chars = line.chars().collect::<Vec<char>>();
 
@@ -62,13 +54,11 @@ fn main() {
             while i + z < j {
                 let word = &chars.as_slice()[i + z..j].iter().collect::<String>();
                 if compare(word.to_string(), numbers.clone()) {
-                    let c = str_to_int(word.to_string());
-                    if foundi {
-                        founde = true;
-                        ec = c;
+                    let n = str_to_int(word.to_string());
+                    if first > 0 {
+                        last = n;
                     } else {
-                        foundi = true;
-                        ic = c;
+                        first = n;
                     }
                     break;
                 }
@@ -76,7 +66,7 @@ fn main() {
             }
         }
 
-        let t = calc(ic, ec, foundi, founde);
+        let t = calc(first, last);
 
         // println!("t: {}", t);
         total += t;
@@ -85,55 +75,36 @@ fn main() {
     println!("Total: {}", total);
 }
 
-fn calc(i: char, e: char, fi: bool, fe: bool) -> u32 {
+fn calc(f: u32, l: u32) -> u32 {
     let mut t = 0;
-    if fi && fe {
-        let ni = i.to_digit(10).unwrap();
-        let ne = e.to_digit(10).unwrap();
 
-        if ni > 0 {
-            t += ni * 10
-        }
-
-        if ne > 0 {
-            t += ne
-        }
+    if f > 0 {
+        t += f * 10
+    } else {
+        t += l * 10;
     }
 
-    if fi && !fe {
-        let ni = i.to_digit(10).unwrap();
-        t += (ni * 10) + ni;
-    }
-
-    if !fi && fe {
-        let nj = e.to_digit(10).unwrap();
-        t += (nj * 10) + nj;
+    if l > 0 {
+        t += l
+    } else {
+        t += f;
     }
 
     t
 }
 
-fn str_to_int(s: String) -> char {
+fn str_to_int(s: String) -> u32 {
     match s {
-        s if s == "one" => '1',
-        s if s == "two" => '2',
-        s if s == "three" => '3',
-        s if s == "four" => '4',
-        s if s == "five" => '5',
-        s if s == "six" => '6',
-        s if s == "seven" => '7',
-        s if s == "eight" => '8',
-        s if s == "nine" => '9',
-        s if s == "1" => '1',
-        s if s == "2" => '2',
-        s if s == "3" => '3',
-        s if s == "4" => '4',
-        s if s == "5" => '5',
-        s if s == "6" => '6',
-        s if s == "7" => '7',
-        s if s == "8" => '8',
-        s if s == "9" => '9',
-        _ => '0',
+        s if s == "one" || s == "1" => 1,
+        s if s == "two" || s == "2" => 2,
+        s if s == "three" || s == "3" => 3,
+        s if s == "four" || s == "4" => 4,
+        s if s == "five" || s == "5" => 5,
+        s if s == "six" || s == "6" => 6,
+        s if s == "seven" || s == "7" => 7,
+        s if s == "eight" || s == "8" => 8,
+        s if s == "nine" || s == "9" => 9,
+        _ => 0,
     }
 }
 
