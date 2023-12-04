98b2833443db53291d023c7b5f9f0596630a3ea0
Mario Areias <mario.s.s.areias@gmail.com>
2023-12-04 23:57:55 +1100

    working with test input part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,12 +1,19 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 fn main() {
-    let f = lib::open_file("day4/input.txt".to_string()).unwrap();
+    let f = lib::open_file("day4/test.txt".to_string()).unwrap();
     let lines = lib::read_lines_from_file(f).unwrap();
 
     let mut cards_score = 0;
-    for line in lines {
-        // println!("Line: {}", line);
+    let mut copies_map: HashMap<usize, usize> = HashMap::new();
+
+    let len = lines.len();
+
+    for (i, line) in lines.iter().enumerate() {
+        let card_number = i + 1;
+        let copies = *copies_map.entry(card_number).or_insert(1);
+        println!("Card: {} Copies: {}", card_number, copies);
+
         let mut parts = line.split(':');
         // discard cardnumber
         parts.next();
@@ -21,6 +28,7 @@ fn main() {
         let winning_numbers: HashSet<&str> = HashSet::from_iter(winning.trim().split(' '));
 
         let mut score = 0;
+        let mut matches = 0;
         for number in my_numbers.trim().split(' ') {
             if winning_numbers.contains(number) {
                 if number.is_empty() {
@@ -31,11 +39,31 @@ fn main() {
                 } else {
                     score *= 2;
                 }
+
+                matches += 1;
             }
         }
 
+        println!("Matches: {}", matches);
+
         cards_score += score;
+
+        for j in 1..=matches {
+            let n = card_number + j;
+            if n == len {
+                break;
+            }
+
+            copies_map
+                .entry(card_number + j)
+                .and_modify(|e| *e += copies)
+                .or_insert(2);
+        }
+
+        println!("copies_map: {:?}", copies_map);
     }
 
     println!("Score Total: {}", cards_score);
+    let total = copies_map.values().sum::<usize>();
+    println!("Total: {}", total);
 }
