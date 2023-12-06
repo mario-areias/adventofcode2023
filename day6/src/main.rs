30a55e643b1d7e7a242b06884d499ef96a7fdb61
Mario Areias <mario.s.s.areias@gmail.com>
2023-12-06 20:32:01 +1100

    day 6 part 1

---
+++ day6/src/main.rs
@@ -0,0 +1,53 @@
+fn main() {
+    let f = lib::open_file("day6/input.txt".to_string()).unwrap();
+    let lines = lib::read_lines_from_file(f).unwrap();
+
+    let time: Vec<usize> = lines[0]
+        .split(' ')
+        .map(|s| s.trim())
+        .map(|s| s.parse::<usize>())
+        .filter_map(Result::ok)
+        .collect();
+
+    let distance: Vec<usize> = lines[1]
+        .split(' ')
+        .map(|s| s.trim())
+        .map(|s| s.parse::<usize>())
+        .filter_map(Result::ok)
+        .collect();
+
+    let mut total = 1;
+
+    for i in 0..time.len() {
+        let t = time[i];
+        let d = distance[i];
+        let mut single_total = 0;
+
+        for j in 1..t {
+            let r = (t - j) * j;
+            if r > d {
+                single_total += 1;
+            }
+        }
+
+        println!("single_total: {}", single_total);
+        total *= single_total;
+    }
+
+    println!("total: {}", total);
+}
+
+// x = 7ms
+// y = 9mm
+//
+// 0ms -> 0mm
+// 1ms -> 6mm
+// 2ms -> 10mm
+// 3ms -> 12mm
+// 4ms -> 12mm
+// 5ms -> 10mm
+// 6ms -> 6mm
+// 7ms -> 0mm
+//
+// r = (x - b) * (b)
+// r = (7 - 3) * (3)
